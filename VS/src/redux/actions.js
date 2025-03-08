198eb6cad3b90d704caa953ea315f064e4982ffb
Nainajha09 <shambhavijha2k2@gmail.com>
2025-03-09 00:00:46 +0530

    Add files via upload

---
+++ VS/src/redux/actions.js
@@ -0,0 +1,17 @@
+export const addTask = (task) => ({
+    type: 'ADD_TASK',
+    payload: task,
+  });
+  
+  export const deleteTask = (id) => ({
+    type: 'DELETE_TASK',
+    payload: id,
+  });
+  
+  export const login = () => ({
+    type: 'LOGIN',
+  });
+  
+  export const logout = () => ({
+    type: 'LOGOUT',
+  });
